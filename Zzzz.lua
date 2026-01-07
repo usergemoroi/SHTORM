766a9575136938571296b256d22eead0b10f292b
usergemoroi <forlokdor7@gmail.com>
2026-01-07 19:48:11 +1000

    Create Zzzz.lua

---
+++ Zzzz.lua
@@ -0,0 +1,30 @@
+-- Services
+Local Players =game:Getservice("Players")
+local UserInputservice = game:Getservice("UserInputservice")
+local Tweenservice = game:Getservice("Tweenservice")
+local Httpservice = game:Getservice("Httpservice")
+-- Configuration
+local Player = Players.LocalPlayer
+local Identifier ='"zzzhub2"
+Local UserID HWID = Player.UserId
+-- Script URLS
+Local regularscripturl =
+"https://raw.githubusercontent.com/zzxzsss/zxs/refs/heads/main/main/main.lua"
+local premiumScripturl =
+"https://raw.githubusercontent.com/zzxzsss/zxs/refs/heads/main/premium/premium. lua"
+-- TODO: Replace with your actual premium script URL
+Local scriptToLoad = ispremium and premiumscripturl or regularscripturl
+loadstring(game:HttpGet(scriptToLoad))()
+print (" CRACKED BY : Tsunami Services - Brother this is easy to crack.")
+Local AkaliNotif =
+Loadstring(game:HttpGet("https://raw.githubusercontent.com/CheatUserzz/
+Robloxopensourcescripts/refs/heads/main/TsNotificactionsource. lua"))();
+local Notify = AkaliNotif.Notify;
+wait(1);
+Notify({
+Description ="This shit its easy to crack, brother, add protection please in
+next";
+Title = "Tsunami Services
+Duration =5;
+});
+ZZZ HUB";
